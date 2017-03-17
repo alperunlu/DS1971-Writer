ebf4eaf136369068007353fe15dc1f66db3d3f30
Alper Ünlü <alperunlu@ieee.org>
2017-03-18 02:20:57 +0300

    Update ds1971-writer.ino

--- ds1971-writer/ds1971-writer.ino
+++ ds1971-writer/ds1971-writer.ino
@@ -1,6 +1,6 @@
 //
 //  Maxim Dallas DS1971-F5 IButton Memory Filler
-//  Fills IButton's EEPROM with hardcoded byte. (default : FF)
+//  Fills IButton's EEPROM with a hardcoded byte. (default : FF)
 //  Attach the 5V through 4.7k ohm resistor to pin 7, and connect GND.
 //  Written by Alper Unlu & Okan Erinc Yildirim
 
